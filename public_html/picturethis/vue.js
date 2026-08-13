6708332673d58eba3eddd3b0a67961d6c20c54dd
Houcemeddine Turki <turkiabdelwaheb@hotmail.fr>
2026-08-13 14:23:59 +0100

    Add files via upload

--- public_html/picturethis/vue.js
+++ public_html/picturethis/vue.js
@@ -1,27 +1,24 @@
 'use strict';
 
-let router ;
-let app ;
-let wd = new WikiData() ;
+let router;
+let app;
+const wd = new WikiData();
 
-
-$(document).ready ( function () {
-	vue_components.toolname = 'picturethis' ;
-	Promise.all ( [
-		vue_components.loadComponents ( ['wd-link','widar','tool-translate','tool-navbar','main.html'] ) ,
-//		new Promise(function(resolve, reject) { resolve() } )
-	] )	.then ( () => {
-			widar_api_url = 'https://wikishootme.toolforge.org/api_v3.php' ;
-			wd_link_wd = wd ;
-			const routes = [
-			  { path: '/', component: MainPage , props:true },
-			  { path: '/:query', component: MainPage , props:true },
-			] ;
-			router = new VueRouter({routes}) ;
-			app = new Vue ( { router } ) .$mount('#app') ;
-		} ) ;
+$(document).ready(() => {
+	vue_components.toolname = 'picturethis';
+	Promise.all([
+		vue_components.loadComponents(['wd-link', 'widar', 'tool-translate', 'tool-navbar', 'main.html'])
+	]).then(() => {
+		widar_api_url = 'https://wikishootme.toolforge.org/api_v3.php';
+		wd_link_wd = wd;
+		const routes = [
+			{ path: '/', component: MainPage, props: true },
+			{ path: '/:query', component: MainPage, props: true },
+		];
+		router = new VueRouter({ routes });
+		app = new Vue({ router }).$mount('#app');
+	});
 
 	// Logging
-	$.getJSON ( 'https://tools.wmflabs.org/magnustools/logger.php?tool=picturethis&method=loaded&callback=?' , function(j){} ) ;
-
-} ) ;
+	$.getJSON('https://magnustools.toolforge.org/logger.php?tool=picturethis&method=loaded&callback=?', () => { });
+});
